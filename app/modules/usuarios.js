fcea6565fcaaeeb04ddeb4dfa03f9b6bf610d987
Dixon Pedraza <pedrazadixon@gmail.com>
2021-11-28 18:03:15 -0500

    avances en modulo de usuarios

--- app/modules/usuarios.js
+++ app/modules/usuarios.js
@@ -4,13 +4,13 @@ const Model = require("../models/Usuario");
 
 const router = express.Router();
 
-// lista de usuarios
+// listar todos
 router.get("/", async (req, res) => {
   let usuarios = await Model.find();
   resp.ok(res, usuarios);
 });
 
-// crear nuevo usuario
+// crear
 router.post("/", async (req, res) => {
   try {
     let usuario = new Model(req.body);
@@ -19,6 +19,20 @@ router.post("/", async (req, res) => {
   } catch (error) {
     resp.error(res, error.message);
   }
+});
+
+// eliminar
+router.delete("/:id", async (req, res) => {
+  await Model.findByIdAndDelete(req.params.id);
+  resp.ok(res, "deleted");
+});
+
+// editar
+router.put("/:id", async (req, res) => {
+  delete req.body.contrasena;
+  await Model.findByIdAndUpdate(req.params.id, req.body);
+  const usuario = await Model.findById(req.params.id);
+  resp.ok(res, usuario);
 });
 
 module.exports = router;
