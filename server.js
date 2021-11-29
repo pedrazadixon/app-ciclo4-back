59dbb5dd67cfcc67d2de12af99293e0cf1f53776
Dixon Pedraza <pedrazadixon@gmail.com>
2021-11-28 19:05:09 -0500

    seteo puerto de dyno

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const ordenes = require("./app/modules/ordenes");
 const destinos = require("./app/modules/destinos");
 
 const app = express();
-const port = process.env.API_PORT;
+const port = process.env.PORT || 3000;
 app.use(express.json());
 
 // db mongo
